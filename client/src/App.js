9399863dbddfaa80be0e053f4d1280fe6d94988f
PAVANKUMAR <pavankumar3cu@gmail.com>
2025-04-14 15:26:32 +0530

    modifed in App.js and App.css

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
-     <h1>Brn Infotech</h1>
-     <h1>Parvatha Chari</h1>
-     <h1>Trica Jasper</h1>
+     <h1>Local Repository : </h1>
+     <p>To pull from the remote Repository</p>
+     <h1>Remote Repository :</h1>
+     <p>To push from the Local repository </p>
+     <h1>Push :</h1>
+     <p>To add the changes from remote repository by using push</p>
+     <h1>Pull :</h1>
+     <p>To get the latest upades from remote repository by using pull.</p>
     </div>
   );
 }
