ef77d032f3f672f801d8836635c272c7c9b4edcf
Pavankumar9cu <pavankumar3cu@gmail.com>
2025-04-14 14:53:23 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+     <h1>Hello</h1>
      <h1>Brn Infotech</h1>
      <h1>Parvatha Chari</h1>
      <h1>Trica Jasper</h1>
